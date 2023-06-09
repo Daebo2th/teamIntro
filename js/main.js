dd9763f4c8fc606e93d149e96ddcda1a35b522dc
Song014 <ghgsb6200@gmail.com>
2023-06-09 16:30:03 +0900

    add all

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+const contentEl = document.querySelector('.content')
+const asideEl = contentEl.querySelector('.aside-menu')
+const showEl = contentEl.querySelector('.show')
+
+asideEl.addEventListener('click',function (e){
+    gsap.to(showEl,.2,{
+        backgroundImage: `url(${e.target.src})`,
+        backgroundSize: "550px",
+        backgroundRepeat: "no-repeat",
+    })
+})
