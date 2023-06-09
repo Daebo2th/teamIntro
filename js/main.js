7416d9727d463cae13b3d48d27d54eef97a391b4
Song014 <ghgsb6200@gmail.com>
2023-06-09 17:54:17 +0900

    show 리뉴ìì얼

--- js/main.js
+++ js/main.js
@@ -14,12 +14,11 @@ var swiper = new Swiper(".mySwiper", {
 
 const contentEl = document.querySelector('.content')
 const asideEl = contentEl.querySelector('.aside-menu')
-const showEl = contentEl.querySelector('.show')
+
 
 asideEl.addEventListener('click',function (e){
-    gsap.to(showEl,.2,{
-        backgroundImage: `url(${e.target.src})`,
-        backgroundSize: "550px",
-        backgroundRepeat: "no-repeat",
-    })
+    var clicked = e.target.dataset.big;
+    const clickedEl = document.querySelector(clicked)
+    $('.show').find('.introduce').removeClass('selected')
+    clickedEl.classList.add('selected')
 })
